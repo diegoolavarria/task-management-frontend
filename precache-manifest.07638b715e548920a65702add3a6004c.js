ea6a9236e0cc8d95646507be096b50474a547f11
diegoolavarria <diegoolavarria@users.noreply.github.com>
2021-09-13 00:11:03 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.07638b715e548920a65702add3a6004c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0faeef92780b3622f89183a8af8a74",
+    "url": "https://diegoolavarria.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "337170dbc84c033fc257",
+    "url": "https://diegoolavarria.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "60073ff7ed66e6262773",
+    "url": "https://diegoolavarria.github.io/task-management-frontend/static/js/2.6daf1d26.chunk.js"
+  },
+  {
+    "revision": "337170dbc84c033fc257",
+    "url": "https://diegoolavarria.github.io/task-management-frontend/static/js/main.1cc6cc7b.chunk.js"
+  },
+  {
+    "revision": "aa380521415da5e87a8d",
+    "url": "https://diegoolavarria.github.io/task-management-frontend/static/js/runtime~main.fde7b89c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://arielweinberger.github.io/task-management-frontend/precache-manifest.0a88baa7b4ebb92ea89c64cd3e3f4832.js"
+  "https://diegoolavarria.github.io/task-management-frontend/precache-manifest.07638b715e548920a65702add3a6004c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arielweinberger.github.io/task-management-frontend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://diegoolavarria.github.io/task-management-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
